49e438933a90a4a4894274810b0dc734e84846da
SONCHAEYEON <codus5068@naver.com>
2023-09-12 18:21:07 +0900

    Week_1

---
+++ Week_1/Review2_1_2/Review2_1_2.c
@@ -0,0 +1,64 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+	int midterm;
+	int final;
+} Score;
+
+void printScore(Score* p) {
+	printf("중간고사 성적은 %d\n", p->midterm);
+	printf("학기말고사 성적은 %d\n", p->final);
+}
+
+Score* biggerScore(Score* p1, Score* p2) {
+	if (p1->midterm + p1->final >= p2->midterm + p2->final)
+		return p1;
+	else
+		return p2;
+}
+
+Score* totalScore(Score* p1, Score* p2) {
+	Score* temp;
+	temp = (Score*)malloc(sizeof(Score));
+	temp->midterm = p1->midterm + p2->midterm;
+	temp->final = p1->final + p2->final;
+	return temp;
+}
+
+Score* createScore(int m, int f) {
+	Score* temp;
+	temp = (Score*)malloc(sizeof(Score));
+	temp->midterm = m;
+	temp->final = f;
+	return temp;
+}
+
+int main(void) {
+	Score s1, s2; // 포인터 대신 구조체 변수
+	Score* p3;
+
+	s1.midterm = 50;
+	s1.final = 100;
+	s2.midterm = 70;
+	s2.final = 70;
+
+	printScore(&s1);
+	printScore(&s2);
+
+	printf("-----------------------------\n");
+	printf("둘 중 성적이 좋은 점수:\n");
+	printScore(biggerScore(&s1, &s2));
+	printf("-----------------------------\n");
+	printf("두 성적의 총합:\n");
+	printScore(totalScore(&s1, &s2));
+	printf("-----------------------------\n");
+
+	p3 = createScore(100, 100);
+	printScore(p3);
+
+	free(p3);
+
+	return 0;
+}
